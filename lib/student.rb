da672d27d00f3e0d5fc82d6c7465d1a3baf7e539
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-17 20:11:16 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,13 @@
-require_relative "./chatty_student.rb"
 
-class Student < ChattyStudent
+class Student 
 
+  def hello
+    puts "Hey there! I'm so excited to learn stuff."
+  end
+
+  def raise_hand
+    puts "Pick me!"
+  end
 
 
 end
