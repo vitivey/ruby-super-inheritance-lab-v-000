a953a729080dfd7a00bc7aa9c2334534b36c1d94
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-17 20:00:35 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,7 +1,9 @@
 class ChattyStudent
 
   def hello
-
+    puts "Hey there! I'm so excited to learn stuff.How are you doi
+ng today? I'm okay, but I'm kind of tired....n, it was so crazy! What, you don't want any s
+poilers? Okay well let me just tell you who died..."
   end
 
   def raise_hand
